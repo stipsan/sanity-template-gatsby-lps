e197268f2e33e4dcaef5c07148113381a66b930e
Chad Mueller <cmueller@rynoss.com>
2021-06-04 15:23:35 -0700

    testing build

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Footer({company, address, phone}) {
     const year = new Date().getFullYear();
     return (
-      <footer class="text-center text-gray-900 text-sm pb-12 pt-9">
+      <footer className="text-center text-gray-900 text-sm pb-12 pt-9">
         <p>&copy; {{ year }} {{ company }} | {{ address }}</p>
-        <p>Call Us Today – {{ phone }} | Marketing by <a class="text-black font-semibold" href="https://rynoss.com" target="_blank">RYNO Strategic Solutions</a></p>
+        <p>Call Us Today – {{ phone }} | Marketing by <a className="text-black font-semibold" href="https://rynoss.com" target="_blank">RYNO Strategic Solutions</a></p>
       </footer>
     );
 }
