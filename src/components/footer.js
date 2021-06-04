9937049a949939849b0d38bf52074fa3add3f74e
Chad Mueller <cmueller@rynoss.com>
2021-06-04 15:19:46 -0700

    update year in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 export default function Footer({company, address, phone}) {
-
+    const year = new Date().getFullYear();
     return (
       <footer class="text-center text-gray-900 text-sm pb-12 pt-9">
-        <p>&copy; {{ new Date().getFullYear() }} {{ company }} | {{ address }}</p>
+        <p>&copy; {{ year }} {{ company }} | {{ address }}</p>
         <p>Call Us Today – {{ phone }} | Marketing by <a class="text-black font-semibold" href="https://rynoss.com" target="_blank">RYNO Strategic Solutions</a></p>
       </footer>
     );
