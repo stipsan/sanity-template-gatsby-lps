fb866aaa743b9025659431f8ffb4d3670d9780d7
Chad Mueller <cmueller@rynoss.com>
2021-07-09 14:32:13 -0700

    Improve hero responsiveness

--- src/components/hero.js
+++ src/components/hero.js
@@ -11,19 +11,19 @@ const Hero = styled.div`
 
 export default function HeroSection({heroBg, title, subTitle, content, email}){
     return (
-        <Hero id="hero" className="bg-hero bg-no-repeat bg-center bg-top px-4 flex items-center min-h-400" bg={heroBg}>
+        <Hero id="hero" className="bg-hero bg-no-repeat bg-center bg-top px-4 flex items-center min-h-400 retina:bg-cover" bg={heroBg}>
             <div className="container mx-auto flex justify-center tablet:justify-start">
-                <div className="flex-2 text-white flex flex-col justify-center laptop:pr-44 laptop:my-8 desktop:pr-60">
+                <div className="flex-2 text-white flex flex-col justify-center laptop:pr-44 laptop:py-16 desktop:pr-60">
                     <div className="text-4xl font-extrabold uppercase tablet:text-6xl desktop:text-7xl">
                         {title} <br />
                         <span className="font-semibold mt-1 phablet:text-5xl tablet:inline-block">{subTitle}</span>
                     </div>
-                    <p className="font-semibold mt-3 text-base tablet:font-normal tablet:text-xl desktop:mb-20">
+                    <p className="font-semibold mt-3 text-base tablet:font-normal tablet:text-xl">
                         {content}
                     </p>
                 </div>
-                <div className="flex-1 hidden laptop:block">
-                    <div className="py-6 pb-12 px-9 bg-blue-primary rounded-md relative tablet:mt-8 tablet:mt-16 desktop:mt-28">
+                <div className="flex-1 hidden laptop:block laptop:pt-16">
+                    <div className="py-6 pb-12 px-9 bg-blue-primary rounded-md relative tablet:-mb-8 tablet:xmt-16 desktop:xmt-28">
                         <div className="uppercase text-center text-3xl text-white font-extrabold mb-5">
                             Schedule Now
                         </div>
