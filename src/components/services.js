3bf6169d213c690799e3f4a1434a37c508be41fa
Chad Mueller <cmueller@rynoss.com>
2021-07-27 13:09:32 -0700

    Adjust service box styles

--- src/components/services.js
+++ src/components/services.js
@@ -7,19 +7,19 @@ export default function Services({services, color, icon}) {
       <section id="service-ctas" className="bg-gray-custom py-12 tablet:pt-24 tablet:pb-36">
         <div className="container mx-auto text-center">
           <Ornament color={color} icon={icon} alt={'Company Icon'} />
-          <h2 className="text-blue-primary text-magic laptop:text-5xl font-extrabold mb-8">We Can Assist You With:</h2>
+          <h2 className="text-blue-primary text-magic laptop:text-5xl font-bold mb-8">We Can Assist You With:</h2>
           <div className="flex flex-wrap justify-center mx-auto">
             {
                 services.map((service) => {
-                    return <div className="flex-grow-0 flex-shrink-0 flex justify-center items-center flex-col capitalize py-4 px-6 m-4 phablet:m-7 bg-white rounded-md phablet:px-10 phablet:py-6">
+                    return <div className="flex-grow-0 flex-shrink-0 flex justify-center items-center flex-col capitalize py-5 px-6 m-3 phablet:m-4 bg-white rounded-md phablet:px-11 phablet:py-4">
                         <div><img className="mb-4" src={service.icon.asset.url} alt={`${service.label} Icon`} /></div>
-                        <div className="w-32">{service.label}</div>
+                        <div className="w-36 text-xl leading-7 tracking-tighter font-semibold">{service.label}</div>
                     </div>
                 })
             }
           </div>
           <AnchorLink
-              className="uppercase rounded-xl px-8 mt-5 py-3 text-2xl bg-yellow-primary font-extrabold mx-auto inline-block"
+              className="uppercase rounded-xl px-10 mt-5 py-3 text-2xl bg-yellow-primary font-extrabold mx-auto inline-block"
               to="#contact" title="Contact Us">
               Ready To Contact Us?
           </AnchorLink>
