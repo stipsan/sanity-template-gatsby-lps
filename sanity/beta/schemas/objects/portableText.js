6e7b29b9641e15bbb08c4d4c70314aa52650559d
Chad Mueller <cmueller@rynoss.com>
2021-08-27 16:21:50 -0700

    Add portable text object

--- sanity/beta/schemas/objects/portableText.js
+++ sanity/beta/schemas/objects/portableText.js
@@ -8,16 +8,18 @@ export default {
         type: 'block',
         styles: [
           { title: 'Normal', value: 'normal' },
+          { title: 'H1', value: 'h1' },
           { title: 'H2', value: 'h2' },
           { title: 'H3', value: 'h3' },
           { title: 'H4', value: 'h4' },
-          { title: 'Quote', value: 'blockquote' },
+          { title: 'H5', value: 'h5' },
+          { title: 'H6', value: 'h6' },
+          //{ title: 'Quote', value: 'blockquote' },
         ],
         marks: {
           decorators: [
             { title: 'Strong', value: 'strong' },
             { title: 'Emphasis', value: 'em' },
-            { title: 'Code', value: 'code' },
           ],
         },
       },
