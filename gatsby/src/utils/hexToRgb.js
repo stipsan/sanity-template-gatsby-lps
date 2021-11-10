0f7a575e76b63f168cb90d7f15323b6cab7f9fc9
Chad Mueller <cmueller@rynoss.com>
2021-11-10 15:24:34 -0700

    Check that hex is at least defined, more thorugh testing coming soon

--- gatsby/src/utils/hexToRgb.js
+++ gatsby/src/utils/hexToRgb.js
@@ -1,6 +1,10 @@
 export default function hexToRgb(hex){
-    return hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i
-    ,(m, r, g, b) => '#' + r + r + g + g + b + b)
-    .substring(1).match(/.{2}/g)
-    .map(x => parseInt(x, 16))
+    if (hex === undefined) {
+        return 'Hex not provided';
+    }
+    return hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i,
+        (m, r, g, b) => '#' + r + r + g + g + b + b)
+        .substring(1).match(/.{2}/g)
+        .map(x => parseInt(x, 16)
+    )
 }
