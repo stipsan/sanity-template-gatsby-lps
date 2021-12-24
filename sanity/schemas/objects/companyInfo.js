5466b0f1d8fbf77d9f3cefda6b656793989e92ff
Chad Mueller <cmueller@rynoss.com>
2021-12-23 21:03:03 -0700

    Disable collapsing company info due to bug from sanity update

--- sanity/schemas/objects/companyInfo.js
+++ sanity/schemas/objects/companyInfo.js
@@ -7,7 +7,7 @@ export default {
             name: 'companyInfo',
             options: {
                 collapsible: true, // Makes the whole fieldset collapsible
-                collapsed: true, // Defines if the fieldset should be collapsed by default or not
+                collapsed: false, // Defines if the fieldset should be collapsed by default or not
                 columns: 1 // Defines a grid for the fields and how many columns it should have
             },
         },
