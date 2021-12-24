31d97cfca21ad0832ab0a7a0b4d769262bf27b38
Chad Mueller <cmueller@rynoss.com>
2021-12-23 20:59:18 -0700

    Fixes variable order bug after refactor

--- gatsby/src/utils/lightenDarkenColor.ts
+++ gatsby/src/utils/lightenDarkenColor.ts
@@ -1,9 +1,5 @@
-export default function lightenDarkenColor(hex:string, amt:number):string {
+export default function lightenDarkenColor(hex:string, amt:number): string {
   let usePound:boolean = false;
-  let num:number = parseInt(hex, 16);
-  let r:number = (num >> 16) + amt;
-  let b:number = ((num >> 8) & 0x00ff) + amt;
-  let g:number = (num & 0x0000ff) + amt;
 
   if (hex === undefined) {
     return;
@@ -14,20 +10,27 @@ export default function lightenDarkenColor(hex:string, amt:number):string {
     usePound = true;
   }
 
+  let num:number = parseInt(hex, 16);
+  let r:number = (num >> 16) + amt;
   if (r > 255) {
      r = 255;
   } else if (r < 0) {
     r = 0;
   }
 
+  let b:number = ((num >> 8) & 0x00ff) + amt;
   if (b > 255) {
     b = 255;
   } else if (b < 0) {
     b = 0;
   }
 
-  if (g > 255) g = 255;
-  else if (g < 0) g = 0;
-
+  let g:number = (num & 0x0000ff) + amt;
+  if (g > 255) {
+    g = 255;
+  } else if (g < 0) {
+    g = 0;
+  }
+  
   return (usePound ? '#' : '') + (g | (b << 8) | (r << 16)).toString(16);
 }
