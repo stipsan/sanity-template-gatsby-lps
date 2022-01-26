643973255f40af6f029216847e85ca7aa1d9a86a
Chad Mueller <cmueller@rynoss.com>
2022-01-25 18:51:44 -0700

    Testing lint-staged

--- web/src/components/contact.js
+++ web/src/components/contact.js
@@ -8,8 +8,12 @@ export default function Contact({ email }) {
         <h2 className="font-semibold text-center mb-7 text-3xl phablet:text-3xl tablet:text-5xl desktop:text-6xl xretina:text-7xl">
           Contact Our Specialists Today
         </h2>
-        <FullwidthForm recipient={email} isSplit="true" />
+                              <FullwidthForm recipient={email} isSplit="true" />
       </div>
     </section>
   );
 }
+
+
+
+
