b0574196e9fe0b43653804803b81b05562e11cc0
Chad Mueller <cmueller@Chads-MacBook-Pro-2.local>
2022-02-11 03:02:36 -0700

    Add netlify dash widget plugin and config

---
+++ studio/dashboardConfig.js
@@ -0,0 +1,24 @@
+export default {
+    widgets: [
+        {
+        name: 'netlify',
+        options: {
+          title: 'Netlify deploys',
+          sites: [
+            // {
+            //   title: 'Sanity Studio',
+            //   apiId: '<#<deployments.studio.providerInfo.siteId>#>',
+            //   buildHookId: '<#<deployments.studio.providerInfo.buildHookId>#>',
+            //   name: '<#<deployments.studio.providerInfo.siteName>#>',
+            // },
+            {
+              title: 'Web Frontend',
+              apiId: '<#<deployments.web.providerInfo.siteId>#>',
+              buildHookId: '<#<deployments.web.providerInfo.buildHookId>#>',
+              name: '<#<deployments.web.providerInfo.siteName>#>',
+            }
+          ]
+        }
+      }
+    ]
+}
