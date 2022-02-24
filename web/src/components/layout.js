98aa6318da41ec3db950b62961cab670bc8d33f4
Chad Mueller <cmueller@Chads-MacBook-Pro-2.local>
2022-02-24 00:12:48 -0700

    Fix clr casing bug

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -179,7 +179,7 @@ export default function Layout({ sanityData, children }) {
           heroHideForm,
         }}  
       >
-        <SidebarForm recipients={emailRecipients} hasCLR={hasClr} />
+        <SidebarForm recipients={emailRecipients} hasClr={hasClr} />
       </Hero>
       <Intro {...{ introText }} />
       <CalloutBar {...{ calloutMessage, calloutBadge, calloutGradient }} />
@@ -204,7 +204,7 @@ export default function Layout({ sanityData, children }) {
               <h2 className="font-semibold text-center mb-7 leading-9 text-2xl phablet:text-3xl tablet:text-4xl laptop:text-5xl desktop:text-6xl">
                 Contact Our <span className="font-bold">Specialists Today</span>
               </h2>
-              <FullwidthForm recipients={emailRecipients} isSplit={false} hasCLR={hasClr} />
+              <FullwidthForm recipients={emailRecipients} isSplit={false} hasClr={hasClr} />
             </div>
           )}
         </Left>
@@ -221,7 +221,7 @@ export default function Layout({ sanityData, children }) {
                 </Right>
             </Split> */}
       <Contact>
-        <FullwidthForm recipients={emailRecipients} isSplit="true" hasCLR={hasClr} />
+        <FullwidthForm recipients={emailRecipients} isSplit="true" hasClr={hasClr} />
       </Contact>
       <Tagline {...{ tagline, lineColor, iconSrc }} />
       <Badges badges={badgeObjs} />
