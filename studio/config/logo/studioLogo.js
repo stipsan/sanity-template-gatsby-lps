a78de508d9dc68ad8cdaefdc916ef5957ef92c44
Chad Mueller <cmueller@Chads-MacBook-Pro-2.local>
2022-03-09 23:42:36 -0700

    Initial studio branding

---
+++ studio/config/logo/studioLogo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './studioLogo.css';
+
+const myLogo = () => (
+  <img className={styles.studioLogo} src="https://rynoss.com/wp-content/uploads/RYNOfav.png" alt="RYNOSS"/>
+);
+
+export default myLogo;
