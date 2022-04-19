2ae3b8426f211dac331a4fac83144da7ef06b20c
Chad Mueller <cmueller@Chads-MacBook-Pro-2.local>
2022-04-19 16:54:11 -0700

    Update 404 page content

--- web/src/pages/404.js
+++ web/src/pages/404.js
@@ -29,24 +29,7 @@ const NotFoundPage = () => {
   return (
     <main style={pageStyles}>
       <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{' '}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{' '}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === 'development' ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+      <h1 style={headingStyles}>404 Page not found</h1>
     </main>
   );
 };
