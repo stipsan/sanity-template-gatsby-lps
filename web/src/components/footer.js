56fe6e75a08fcdccbe8b0b9be8d4b756c030c313
Chad Mueller <cmueller@Chads-MacBook-Pro-3.local>
2022-06-02 18:05:53 -0700

    make footer phone clickable

--- web/src/components/footer.js
+++ web/src/components/footer.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 export default function Footer({ company, address, phone }) {
   const year = new Date().getFullYear();
+  const phoneLink = <a href={`tel:${phone}`}>{phone}</a>;
   return (
     <footer className="text-center text-gray-alt text-sm pb-12 pt-6">
       <p>
         &copy; {year} {company} {address ? `| ${address}` : ''}
       </p>
       <p>
-        Call Us Today – {phone ? `${phone} |` : ''} Marketing by{' '}
+         {phone ? ['Call Us Today – ', phoneLink, ' |'] : ''} Marketing by{' '}
         <a
           className="font-bold"
           href="https://rynoss.com"
