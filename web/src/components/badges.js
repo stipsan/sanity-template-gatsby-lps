b244c24f044b658c44db96add92f626fae10e6a4
ryno-cmueller <cmueller@rynoss.com>
2022-10-06 13:32:59 -0700

    center badges

--- web/src/components/badges.js
+++ web/src/components/badges.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Badges({ badges }) {
   return (
     <section id="badges">
-      <div className="container mx-auto flex flex-wrap justify-around items-center pb-9 desktop:gap-6">
+      <div className="container mx-auto flex flex-wrap justify-center items-center pb-9 desktop:gap-6">
         {badges.map((badge, index) => (
           <div
             className="flex justify-center py-3 w-full phablet:w-1/2 phablet:py-5 laptop:w-64 desktop:shrink-0 desktop:grow-0"
